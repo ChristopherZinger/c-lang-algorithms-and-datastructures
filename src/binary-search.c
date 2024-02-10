6be7bd122334e8e1d6a234a858ced769883f252c
christopher z <krzysztof.zinger@gmail.com>
2024-02-10 21:14:35 +0100

    simplify binarysearch

--- src/binary-search.c
+++ src/binary-search.c
@@ -1,26 +1,22 @@
-#include <stdio.h>
+#include "binary-search.h"
 
 int binarySearch (int target, int arr[], int size) {
-	int startIndex = 0;
-	int endIndex = size;
+	int lo = 0;
+	int hi = size;
 
 	while(1) {
-		if(startIndex == endIndex) {
-			if (target == arr[startIndex]) {
-				return startIndex;
-			} else {
-				return -1;
-			}
+		if(lo > hi) { 
+		    return -1;
 		}
 
-		int pivotIndex = startIndex + ((endIndex - startIndex) / 2);
+		int pivotIndex = lo + ((hi - lo) / 2);
 		int pivotValue = arr[pivotIndex];
 		if (pivotValue == target)  {
 			return pivotIndex;
 		} else if (pivotValue > target) {
-			endIndex = pivotIndex - 1;
+			hi = pivotIndex - 1;
 		} else {
-			startIndex = pivotIndex + 1;
+			lo = pivotIndex + 1;
 		}
 	}
 }
